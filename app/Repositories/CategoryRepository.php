ce3b5d5aef0587d321e6a30d7125ed3bbbd9893c
shivusoumya <shivadatacamp@gmail.com>
2021-01-10 22:04:59 +0530

    --- Auto-Git Commit ---

---
+++ app/Repositories/CategoryRepository.php
@@ -0,0 +1,42 @@
+<?php
+namespace App\Repositories;
+
+use App\Models\Category;
+use App\Traits\UploadAble;
+use Illuminate\Http\UploadedFile;
+use App\Contracts\CategoryContract;
+use Illuminate\Database\QueryException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Doctrine\Instantiator\Exception\InvalidArgumentException;
+
+/**
+ * Class CategoryRepository
+ *
+ * @package \App\Repositories
+ */
+class CategoryRepository extends BaseRepository implements CategoryContract
+{
+    use UploadAble;
+
+    public function __construct(Category $model)
+    {
+        parent::__construct($model);
+        $this->model = $model;
+    }
+
+    public function listCategories(string $order = 'id', string $sort = 'desc', array $columns = ['*'])
+    {
+        return $this->all($columns, $order, $sort);
+    }
+
+    public function findCategoryById(int $id)
+    {
+        try {
+            return $this->findOneOrFail($id);
+
+        } catch (ModelNotFoundException $e) {
+
+            throw new ModelNotFoundException($e);
+        }
+    }
+}
--- routes/admin.php
+++ routes/admin.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\LoginController;
 use App\Http\Controllers\Admin\SettingController;
+use App\Http\Controllers\Admin\CategoryController;
 
     Route::group(['prefix' => 'admin'], function() {
 
@@ -18,7 +19,18 @@ use App\Http\Controllers\Admin\SettingController;
             })->name('admin.dashboard');
 
             Route::get('/settings', [SettingController::class, 'index'])->name('admin.settings');
-        Route::post('/settings', [SettingController::class, 'update'])->name('admin.settings.update');
+            Route::post('/settings', [SettingController::class, 'update'])->name('admin.settings.update');
+
+            Route::group(['prefix'  =>   'categories'], function() {
+
+                Route::get('/', [CategoryController::class, 'index'])->name('admin.categories.index');
+                Route::get('/create', [CategoryController::class, 'create'])->name('admin.categories.create');
+                Route::post('/store', [CategoryController::class, 'store'])->name('admin.categories.store');
+                Route::get('/{id}/edit', [CategoryController::class, 'edit'])->name('admin.categories.edit');
+                Route::post('/update', [CategoryController::class, 'update'])->name('admin.categories.update');
+                Route::get('/{id}/delete', [CategoryController::class, 'delete'])->name('admin.categories.delete');
+
+            });
 
         });
 
